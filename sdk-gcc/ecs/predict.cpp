2ed04e7ac76a79a759c9fea070f5ee14f4491807
zlseu_edu <zlseu_edu@yahoo.com>
2018-03-13 22:02:05 +0800

    first version.

--- sdk-gcc/ecs/predict.cpp
+++ sdk-gcc/ecs/predict.cpp
@@ -1,12 +1,102 @@
 #include "predict.h"
+
 #include <stdio.h>
 
-//ÄãÒªÍê³ÉµÄ¹¦ÄÜ×ÜÈë¿Ú
+//ï¿½ï¿½Òªï¿½ï¿½ÉµÄ¹ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int data_num, char * filename)
 {
-	// ĞèÒªÊä³öµÄÄÚÈİ
+	// ï¿½ï¿½Òªï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 	char * result_file = (char *)"17\n\n0 8 0 20";
+	Info information = parseInfo(info);
+	Record **record = parseRecord(data, data_num);
 
-	// Ö±½Óµ÷ÓÃÊä³öÎÄ¼şµÄ·½·¨Êä³öµ½Ö¸¶¨ÎÄ¼şÖĞ(psÇë×¢Òâ¸ñÊ½µÄÕıÈ·ĞÔ£¬Èç¹ûÓĞ½â£¬µÚÒ»ĞĞÖ»ÓĞÒ»¸öÊı¾İ£»µÚ¶şĞĞÎª¿Õ£»µÚÈıĞĞ¿ªÊ¼²ÅÊÇ¾ßÌåµÄÊı¾İ£¬Êı¾İÖ®¼äÓÃÒ»¸ö¿Õ¸ñ·Ö¸ô¿ª)
+	int * result = predict_vm(information, record, data_num);
+	for(int i = 0; i < 16; ++i)
+		printf("%d ", result[i]);
+	// Ö±ï¿½Óµï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä¼ï¿½ï¿½Ä·ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ö¸ï¿½ï¿½ï¿½Ä¼ï¿½ï¿½ï¿½(psï¿½ï¿½×¢ï¿½ï¿½ï¿½Ê½ï¿½ï¿½ï¿½ï¿½È·ï¿½Ô£ï¿½ï¿½ï¿½ï¿½ï¿½Ğ½â£¬ï¿½ï¿½Ò»ï¿½ï¿½Ö»ï¿½ï¿½Ò»ï¿½ï¿½ï¿½ï¿½ï¿½İ£ï¿½ï¿½Ú¶ï¿½ï¿½ï¿½Îªï¿½Õ£ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ğ¿ï¿½Ê¼ï¿½ï¿½ï¿½Ç¾ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½İ£ï¿½ï¿½ï¿½ï¿½ï¿½Ö®ï¿½ï¿½ï¿½ï¿½Ò»ï¿½ï¿½ï¿½Õ¸ï¿½Ö¸ï¿½)
 	write_result(result_file, filename);
+}
+
+int* predict_vm(Info info, Record **record, int data_num) { 
+	int *vm = (int*)malloc(sizeof(int) * 16); 
+	int test_time = info.m_endTime - info.m_startTime;
+	int train_time = record[data_num-1]->m_time - record[0]->m_time;
+	printf("test_time %d, train_time %d\n", test_time, train_time);
+	int sum = 0;
+	for (int i=0; i<data_num; i++){
+		for (int j=0; j<info.m_numOfVM; j++)
+			if (record[i]->m_idOfVM==info.m_vm[j]->m_id) {
+				vm[j]++;
+				break;
+			}
+	}
+
+	for(int j = 0; j< info.m_numOfVM; j++){
+		vm[j] = vm[j]*test_time/train_time;
+	}
+	return vm;
+}
+
+Machine parseMachine(char *machine){
+	Machine m;
+	sscanf(machine, "%d %d %d", &m.m_cpu, &m.m_mem, &m.m_hard);
+	printf("%d %d %d\n", m.m_cpu, m.m_mem, m.m_hard);
+	return m;
+}
+
+time_t parseTime(char *time){
+	tm t;
+	sscanf(time, "%d-%d-%d %d:%d:%d", &t.tm_year, &t.tm_mon, &t.tm_mday, &t.tm_hour, &t.tm_min, &t.tm_sec);
+	//printf("%d-%d-%d %d:%d:%d\n", t.year, t.month, t.day, t.hour, t.minute, t.second);
+	return mktime(&t);
+}
+
+VirtualMachine* parseVirtualMachine(char *vm){
+	VirtualMachine *virtualmachine = (VirtualMachine*)malloc(sizeof(VirtualMachine));
+	sscanf(vm, "flavor%d %d %d", &virtualmachine->m_id, &virtualmachine->m_cpu, &virtualmachine->m_mem);
+	printf("%d %d %d\n", virtualmachine->m_id, virtualmachine->m_cpu, virtualmachine->m_mem);
+	return virtualmachine;
+}
+
+Info parseInfo(char *info[MAX_INFO_NUM]){
+	Info information;
+
+	int index = 0;
+	Machine m = parseMachine(info[index]);
+	information.m_machine.m_cpu = m.m_cpu;
+	information.m_machine.m_mem = m.m_mem; 
+	information.m_machine.m_hard = m.m_hard;
+
+	index = 2;
+	int numOfVM = 0;
+	sscanf(info[index], "%d", &numOfVM);
+	information.m_numOfVM = numOfVM;
+
+	information.m_vm = (VirtualMachine**)malloc(sizeof(VirtualMachine*) * numOfVM);
+
+	index = 3;
+	for(int i = 0; i < numOfVM; ++i){
+		information.m_vm[i] = parseVirtualMachine(info[index]);
+		++index;
+	}
+
+	// è·³è¿‡cpuè¿™ä¸€è¡Œ
+	index += 3;
+	information.m_startTime = parseTime(info[index]);
+	information.m_endTime = parseTime(info[++index]);
+
+	return information;
+}
+
+Record** parseRecord(char * data[MAX_DATA_NUM], int data_num){
+	Record **r = (Record**)malloc(sizeof(Record*) * data_num);
+
+	for(int i = 0; i < data_num; ++i){
+		r[i] = (Record*)malloc(sizeof(Record));
+		char time[50];
+		sscanf(data[i], "%s flavor%d %s %s", r[i]->m_idOfRecord, &r[i]->m_idOfVM, time, time + 11);
+		time[10] = '\t';
+		r[i]->m_time = parseTime(time);
+	}
+	return r;
 }
--- sdk-gcc/ecs/predict.h
+++ sdk-gcc/ecs/predict.h
@@ -3,8 +3,50 @@
 
 #include "lib_io.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct{
+    int m_id;
+    int m_cpu;
+    int m_mem;
+} VirtualMachine;
+
+typedef struct{
+    int m_cpu;
+    int m_mem;
+    int m_hard;
+} Machine;
+
+typedef struct{
+    Machine m_machine;
+
+    int m_numOfVM;
+    VirtualMachine ** m_vm;
+
+    time_t m_startTime;
+    time_t m_endTime;    
+} Info;
+	
+typedef struct{
+    char m_idOfRecord[20];
+    int m_idOfVM;
+    time_t m_time;
+} Record;
+
 void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int data_num, char * filename);
 
-	
+Info parseInfo(char *info[MAX_INFO_NUM]);
+
+Record** parseRecord(char * data[MAX_DATA_NUM], int data_num);
+
+Machine parseMachine(char *machine);
+
+time_t parseTime(char *time);
+
+VirtualMachine* parseVirtualMachine(char *vm);
+
+int* predict_vm(Info info, Record **record, int data_num);
 
 #endif
